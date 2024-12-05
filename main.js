12a8ad0180e4f9b1cb073d976e134027fde85d33
AlexJohannson <mehedsanya@gmail.com>
2024-12-05 12:27:04 +0100

    [update]:Test JavaScript/ main.js,

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*------------------------------------------    DOM  елементи    ---------------------------------------------*/
 /*оримую всі елементи за їх індентифікаторами*/
 const inputNameValue = document.getElementById('input_name-value');
 const listNameValue = document.getElementById('list_name-value');
@@ -6,27 +7,27 @@ const sortByName = document.getElementById('sort_by-name');
 const sortByValue = document.getElementById('sort_by-value');
 const deleteButton = document.getElementById('delete_button');
 
-
-
+/*----------------------------------------    addButton    -----------------------------------------------------*/
 /*створюю обробник події для кнопки "Add"*/
 addButton.addEventListener('click', () => {
 
-    /*отримується значення введене від користувача*/
+    /*створюю константу inputName в яку буде  збережене значення введене від користувача*/
     const inputName = inputNameValue.value;
 
-    /*створюю новий елемент <ol></ol>*/
+    /*створюю константу olListInput яка буде зберігати посилання на створений новий елемент <ol></ol>*/
     const olListInput = document.createElement('ol');
 
     /*додаю клас до створеного елемента <ol></ol>*/
     olListInput.classList.add('olListInput');
 
-    /*створюю новий елемент <li></li>*/
+    /*створюю константу liListItem яка буде зберігати посилання на створений новий елемент <li></li>*/
     const liListItem = document.createElement('li');
 
     /*додаю клас до створеного елемента <li></li>*/
     liListItem.classList.add('liListItem');
 
-    /*додаю обробник подій для кліку на елемент <li></li>*/
+/*--------------------------------------   обробник події liListItem, створюю клас listRemove   ------------------*/
+    /*додаю обробник події для кліку на елемент <li></li>*/
     liListItem.addEventListener('click', (event) => {
 
         /*додаю клас для позначення елемента <li></li> який потрібно видалити*/
@@ -39,6 +40,7 @@ addButton.addEventListener('click', () => {
         console.log(liListItem);
     });
 
+/*------------------------------------------  перевірка формату введеного тексту  ---------------------------------*/
     /*перевіряю формат введеного тексту, користувач може додати пробіл піля значення "<name>" та перед зназенням
     * "<value>". Додавання пробілів перед значенням "<name>" та після значення "<value>" призведе до попередження
     * користувача що формат введення значень "<name>=<value>" не вірний*/
@@ -47,6 +49,7 @@ addButton.addEventListener('click', () => {
       /*у випадку якщо формат не правильний змінюється background текстового поля input_name-value на червоний колір*/
       inputNameValue.style.background = 'red';
     } else {
+
         /*якщо формат введеного тексту правильний тоді повертається стандартний background*/
         inputNameValue.style.background = '#9cbbfc';
 
@@ -59,21 +62,25 @@ addButton.addEventListener('click', () => {
         /*додаю елемент <ol></ol> до <div id="list_name-value"></div>*/
         listNameValue.appendChild(olListInput);
     }
+
     /*очищаю текстове поле після додавання до списку*/
     inputNameValue.value = '';
 });
 
-
-/*створюю обробник подій для кнопки "Sort by Name"*/
+/*------------------------------------------   sortByName   ----------------------------------------------------*/
+/*створюю обробник події для кнопки "Sort by Name"*/
 sortByName.addEventListener('click', () => {
 
-    /*використовую Array.from для перетворення HTML колекції в масив*/
+    /*створюю константу liListName яка буде зберігати масив HTML елементів, використовую Array.from для перетворення HTML
+    колекції в масив отриманої з listNameValue.getElementsByClassName('liListItem')*/
     const liListName = Array.from(listNameValue.getElementsByClassName('liListItem'));
 
     /*сортую масив за значенням "<name>". Використовую метод split який розбиває рядок на масив підрядків символ
     * "=" є роздільником в данній роботі якщо a.innerText="name=value" то a.innerText.split('=') перетворює на
     * масив ["name", "value"]. [0] вибираю перший елемент масиву до знака "=" тобто "name"*/
     liListName.sort((a, b) => {
+
+        /*створю константи liListNameFirst, liListNameSecond для зберігання значень отриманих з масиву*/
         const liListNameFirst = a.innerText.split('=')[0];
         const liListNameSecond = b.innerText.split('=')[0];
 
@@ -82,8 +89,9 @@ sortByName.addEventListener('click', () => {
         * більше liListNameSecond*/
         return liListNameFirst.localeCompare(liListNameSecond);
     });
-    /*використовую forEach для перебору відсортованих елементів і додавання іх назад до <div id="list_name-value">
-    </div>*/
+
+    /*використовую forEach для перебору всіх відсортованих елементів в масиві liListName і додаю відсортовані елементи
+    назад до <div id="list_name-value"></div>*/
     liListName.forEach(sortName => listNameValue.appendChild(sortName));
 
     /*виводжу відсортований масив в консоль*/
@@ -91,23 +99,51 @@ sortByName.addEventListener('click', () => {
 });
 
 
-
+/*------------------------------------------   sortByValue   ----------------------------------------------------*/
+/*створюю обробник події для кнопки "Sort by Value"*/
 sortByValue.addEventListener('click', () => {
+
+    /*створюю константу liListValue яка буде зберігати масив HTML елементів, використовую Array.from для перетворення HTML
+    колекції в масив отриманої з listNameValue.getElementsByClassName('liListItem')*/
     const liListValue = Array.from(listNameValue.getElementsByClassName('liListItem'));
+
+    /*сортую масив за значенням "<value>". Використовую метод split який розбиває рядок на масив підрядків символ
+    * "=" є роздільником в данній роботі якщо a.innerText="name=value" то a.innerText.split('=') перетворює на
+    * масив ["name", "value"]. [1] вибираю другий елемент масиву до знака "=" тобто "value"*/
     liListValue.sort((a, b) => {
+
+        /*створю константи liListValueFirst, liListValueSecond для зберігання значень отриманих з масиву*/
         const liListValueFirst = a.innerText.split('=')[1];
         const liListValueSecond = b.innerText.split('=')[1];
+
+        /*методом localeCompare порівнюю два рядки liListValueFirst та liListValueSecond, метод повертає -1
+        *  якщо liListValueFirst менше liListValueSecond, 0 якщо вони рівні і 1 якщо liListValueFirst
+        * більше liListValueSecond*/
         return liListValueFirst.localeCompare(liListValueSecond);
     });
+
+    /*використовую forEach для перебору всіх відсортованих елементів в масиві liListValue і додаю відсортовані елементи
+    назад до <div id="list_name-value"></div>*/
     liListValue.forEach(sortValue => listNameValue.appendChild(sortValue));
+
+    /*виводжу відсортований масив в консоль*/
     console.log(liListValue);
 });
 
 
-
+/*-----------------------------------------    deleteButton   ---------------------------------------------------*/
+/*створюю обробник події для кнопки "Delete"*/
 deleteButton.addEventListener('click', () => {
+
+    /*створюю константу removeButton яка буде зберігати масив HTML елементів, використовую Array.from для перетворення
+    HTML колекції в масив отриманої з listNameValue.getElementsByClassName('listRemove')*/
     const removeButton = Array.from(listNameValue.getElementsByClassName('listRemove'));
+
+    /*використовую forEach для перебору всіх елементів в масиві removeButton, потім видаляє вибрані елементи
+    користувачем з дерева документа, з HTML сторінки*/
     removeButton.forEach(itemList => itemList.remove());
+
+    /*виводжу видалені елементи в консоль*/
     console.log(removeButton);
 });
 
